404fa8f5abbc676f80eed94bb5cf11abf7a505d6
Begood1 <geze1010@gmail.com>
2018-12-06 00:32:41 +0700

    form

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,32 +1,31 @@
-import '../stylesheets/style.scss';
-import qs from 'qs';
-import axios from 'axios';
-import * as config from '../../config/config.json';
+// import '../stylesheets/style.scss';
+// import qs from 'qs';
+// import axios from 'axios';
+// import * as config from '../../config/config.json';
 
-const form = document.querySelector('.form');
-//https://yero.netlify.com/
-// ðŸ‘‡ Write homework code here
+// const form = document.querySelector('.form');
+// //https://yero.netlify.com/
+// // ðŸ‘‡ Write homework code here
 
-const formData = {
-    name:,
-};
-const postToNetlify = () => {
-    const body = {
-        fields: {
-            Name: name.value,
-            Email: email.value,
-            Comment: comment.value,
-        },
-    };
-    // ðŸ‘† Write homework code here
+// const formData = {
+//     name: name.value
+// };
+// const postToNetlify = () => {
+//     const body = {
+//         fields: {
+//             Name: name.value,
+//             Email: email.value,
+//             Comment: comment.value,
+//         },
+//     };
+//     // ðŸ‘† Write homework code here
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        axios
-            .post(config.site.url, qs.stringify({ 'form-name': 'Comments', ...formData }))
-            .then(_ => console.log('success'))
-            .catch(_ => console.log('failed'));
-    };
+//     const handleSubmit = e => {
+//         e.preventDefault();
+//         axios
+//             .post(config.site.url, qs.stringify({ 'form-name': 'Comments', ...formData }))
+//             .then(_ => console.log('success'))
+//             .catch(_ => console.log('failed'));
+//     };
 
-    form.addEventListener('submit', handleSubmit);
-
+//     form.addEventListener('submit', handleSubmit);
