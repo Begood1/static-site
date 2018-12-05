e2b59f602c49fc9a567bf0df6e4336c44e51e142
Begood1 <geze1010@gmail.com>
2018-12-05 17:53:23 +0700

    commit first mukera

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,3 +1,25 @@
 import '../stylesheets/style.scss';
+import qs from 'qs';
+import axios from 'axios';
+import * as config from '../../config/config.json';
 
+const form = document.querySelector('.form');
+
+// ðŸ‘‡ Write homework code here
+
+const formData = {
+  name: 'Milo',
+};
+
+// ðŸ‘† Write homework code here
+
+const handleSubmit = e => {
+  e.preventDefault();
+  axios
+    .post(config.site.url, qs.stringify({ 'form-name': 'Comments', ...formData }))
+    .then(_ => console.log('success'))
+    .catch(_ => console.log('failed'));
+};
+
+form.addEventListener('submit', handleSubmit);
 console.log('Hello World');
--- events/second.md
+++ events/second.md
@@ -7,3 +7,14 @@ tags:
     - tech
     - sport
 ---
+Description
+Handmade macrame clutch bags with cotton rope 
+
+Boho style clutch bag with fringe tassel.
+Bag is lined .
+
+Measures:10" wide, 8" height (just pouch)
+
+We can also make macrame wall hanging and cushion covers, weeding orcher
+Please let me know your needed, I am glad to make you a special ones,
+
