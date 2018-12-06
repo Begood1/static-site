0d62635cd533688ac0d4f3bd8b388ec60c52ba1e
Begood1 <geze1010@gmail.com>
2018-12-06 17:27:42 +0700

    commit this form

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,31 +1,75 @@
-// import '../stylesheets/style.scss';
-// import qs from 'qs';
-// import axios from 'axios';
-// import * as config from '../../config/config.json';
+import '../stylesheets/style.scss';
+import qs from 'qs';
+import axios from 'axios';
+import * as config from '../../config/config.json';
+
+const form = document.querySelector('.form');
+const email = document.querySelector('.email');
+const comment = document.querySelector('.comment');
+const priority = document.querySelector('input[name="priorty"]:checked').value;
+const formData = {
+  Name: name.value,
+  Email: email.value,
+  Comment: comment.value,
+  priority: priority.value,
+};
+// ðŸ‘† Write homework code here
+
+const handleSubmit = e => {
+  e.preventDefault();
+  axios
+    .post(config.site.url, qs.stringify({ 'form-name': 'config.form.name', ...formData }))
+    .then(_ => console.log('success'))
+    .catch(_ => console.log('failed'));
+};
+
+form.addEventListener('submit', handleSubmit);
 
 // const form = document.querySelector('.form');
-// //https://yero.netlify.com/
-// // ðŸ‘‡ Write homework code here
+// const alert = document.querySelector('.alert');
+// const spinner = document.querySelector('.spinner');
+// const overlay = document.querySelector('.overlay');
+// const name = document.querySelector('.name');
+// const email = document.querySelector('.email');
+// const comment = document.querySelector('.comment');
 
-// const formData = {
-//     name: name.value
+// const clearForm = () => {
+//   name.value = '';
+//   email.value = '';
+//   comment.value = '';
 // };
-// const postToNetlify = () => {
-//     const body = {
-//         fields: {
-//             Name: name.value,
-//             Email: email.value,
-//             Comment: comment.value,
-//         },
-//     };
-//     // ðŸ‘† Write homework code here
 
-//     const handleSubmit = e => {
-//         e.preventDefault();
-//         axios
-//             .post(config.site.url, qs.stringify({ 'form-name': 'Comments', ...formData }))
-//             .then(_ => console.log('success'))
-//             .catch(_ => console.log('failed'));
-//     };
+// const toggleSpinner = () => spinner.classList.toggle('spinner--open');
 
-//     form.addEventListener('submit', handleSubmit);
+// const toggleOverlay = () => overlay.classList.toggle('overlay--open');
+
+// const toggleAlert = type => {
+//   alert.querySelector('span').innerText = type === 'success' ? successMessage : failedMessage;
+//   alert.classList.toggle(`alert--${type}`);
+// };
+
+// const postToAirtable = () => {
+//   const body = {
+//     fields: {
+//       Name: name.value,
+//       Email: email.value,
+//       Comment: comment.value,
+//     },
+//   };
+//   axios
+//     .post(endpoint, body, config)
+//     .then(_ => toggleAlert('success'))
+//     .catch(_ => toggleAlert('failed'))
+//     .then(_ => {
+//       toggleSpinner();
+//       clearForm();
+//       setTimeout(toggleOverlay, 1500);
+//     });
+// };
+
+// const handleSubmit = e => {
+//   e.preventDefault();
+//   toggleOverlay();
+//   toggleSpinner();
+//   postToAirtable();
+// };
